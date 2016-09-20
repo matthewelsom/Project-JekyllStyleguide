2a7cc41558b5df6cbb1519578578879d7a2a27c8
Matthew Elsom <elsom.matthew@innocellence.com>
2016-09-20 10:33:31 +0800

    ADD VIEWPORT SIZE

--- styleguide/js/main.js
+++ styleguide/js/main.js
@@ -24,10 +24,22 @@ function TocButton() {
     $(this).closest('.sg-pusher').toggleClass('hide-toc');
   });
 }
+
+//Get Viewport size
+function ViewportSize() {
+  var iframeWin = document.getElementById('sg-viewport').contentWindow;
+  
+  $(iframeWin).on("resize", function(){
+    var iframe=document.getElementById('sg-viewport');
+    console.log("resize");
+      $('#sg-viewport-size').text(iframe.contentWindow.document.body.scrollHeight  + ' x ' + iframe.contentWindow.document.body.scrollWidth + ' px');
+  });
+}
 //Functions that run when all HTML is loaded
 $(document).ready(function() {
   SmoothScroll();
   TocButton();
+  ViewportSize();
 });
 
 
