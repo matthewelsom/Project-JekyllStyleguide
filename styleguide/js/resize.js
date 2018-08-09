c016d3926d3a7ee4d39b15cbcbd66fcda1b4b3c9
matthewelsom <matthew@matthewelsom.co.uk>
2018-08-09 12:31:54 +0800

    iframe test

--- styleguide/js/resize.js
+++ styleguide/js/resize.js
@@ -5,8 +5,8 @@ iFrameResize({
   inPageLinks             : true, // Enable links in Frame
   bodyMargin              : 0, // Remove body margin
   bodyPadding             : 0, // remove body padding
-  maxWidth: screen.width,
-  minWidth: screen.width,
+//  maxWidth: screen.width,
+//  minWidth: screen.width,
   sizeWidth: 'true'
 });
 
